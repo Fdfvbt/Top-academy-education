8c41842b3469552bc9f11e22421e083d4bca8218
Fdfvbt <120331633+Fdfvbt@users.noreply.github.com>
2023-01-18 17:38:59 +0000

    operators

--- 18.01.23/array.cpp
+++ 18.01.23/array.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+struct nums{
+    int x=2;
+    
+};
+
 class Array{
     int *arr;
     int size;
@@ -50,6 +55,58 @@ void erase(int index){
     }
 }
 
+Array &operator+(int val){
+    add_back(val);
+    return *this;
+}
+
+bool operator<(Array other){
+    return this->size < other.size;
+}
+
+Array &operator-(int val){
+    bool num=false;
+    for(int i =0; i<this->size; i++){
+        if(this->arr[i]==val){
+            erase(i);
+            num = true;
+            break;
+        }
+            
+    }
+    if(num==false){
+        std::cout<<"Error! Number is not found\n";
+    }
+
+}
+
+Array &operator-(const Array &other){
+    if(this->size>other.size){
+        for(int i=0; i<this->size; i++){
+            this->arr[i]-=other.arr[i];
+        }   
+    }else{
+        for(int i=0; i<other.size; i++){
+            this->arr[i]-=other.arr[i];
+        }   
+        
+    }
+}
+bool operator<(const Array &other){
+    return this->size<other.size;
+}
+bool operator>(const Array &other){
+    return this->size>other.size;
+}
+
+
+bool operator>=(const Array &other){
+    return this->size>=other.size;
+}
+bool operator==(const Array &other){
+    return this->size==other.size;
+}
+
 ~Array(){
     delete [] this->arr;
 }
@@ -58,6 +115,12 @@ void erase(int index){
 
 int main(){
     Array arr(15);
+    Array arr2(15);
+
+    arr2.add_back(3);
+    arr2.add_back(2);
+    arr2.add_back(4);
+    arr2.add_back(7);
 
     arr.add_back(1);
     arr.add_back(2);
@@ -76,4 +139,19 @@ int main(){
 
     arr.erase(3);
     arr.print();
+
+    arr+7;
+    arr.print();
+
+    arr-5;
+    arr.print();
+
+    if(arr>arr2){
+        std::cout<<"arr > arr2\n";
+    }else{
+        std::cout<<"arr < arr2\n";
+    }
+    arr-arr2;
+    arr.print();
+
 }
